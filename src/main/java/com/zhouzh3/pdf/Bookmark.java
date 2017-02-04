455282d02171464157c69ef6cc317795f249052e
zzhzzh204553 <zhouzhenhua1984@163.com>
2017-02-04 10:46:47 +0800

    初次上传
    初次上传

---
+++ src/main/java/com/zhouzh3/pdf/Bookmark.java
@@ -0,0 +1,104 @@
+package com.zhouzh3.pdf;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+
+/**
+ * @author Hegel
+ *
+ */
+public class Bookmark {
+
+	private String title;
+
+	private int page;
+
+	private String action = "GoTo";
+
+	private ArrayList<HashMap<String, Object>> kids;
+
+	public Bookmark() {
+		this.kids = new ArrayList<HashMap<String, Object>>();
+	}
+
+	public Bookmark(String title, int page) {
+		this();
+		this.title = title;
+		this.page = page;
+
+	}
+
+	public HashMap<String, Object> getEntry() {
+		HashMap<String, Object> entry = new HashMap<String, Object>();
+		if (title != null && !"".equals(title)) {
+			entry.put("Title", title);
+			entry.put("Action", "GoTo");
+			entry.put("Page", page + " Fit");
+			if (kids.size() != 0) {
+				entry.put("Kids", kids);
+			}
+		}
+
+		return entry;
+	}
+
+	public Bookmark addKid(HashMap<String, Object> entry) {
+		this.kids.add(entry);
+
+		return this;
+	}
+
+	public String getTitle() {
+		return title;
+	}
+
+	public void setTitle(String title) {
+		this.title = title;
+	}
+
+	public String getAction() {
+		return action;
+	}
+
+	public void setAction(String action) {
+		this.action = action;
+	}
+
+	public int getPage() {
+		return page;
+	}
+
+	public void setPage(int page) {
+		this.page = page;
+	}
+
+	public ArrayList<HashMap<String, Object>> getKids() {
+		return kids;
+	}
+
+	@Override
+	public String toString() {
+		return "Bookmark [title=" + title + ", page=" + page + ", action=" + action + "]";
+	}
+
+	public int getLevel() {
+		int retVal = 0;
+		String[] split = this.title.split("[　 ]+", 2);
+		if (split.length == 2) {
+			String section = split[0];
+			retVal = section.split("\\.").length;
+		}
+		return retVal;
+	}
+
+	public static Bookmark buildBookmark(String line, int step) {
+		String[] split = line.split("　(?=\\d)");
+		if (split.length == 2) {
+			return new Bookmark(split[0], Integer.parseInt(split[1]) + step);
+		}
+
+		System.err.println("参数不正确: 【" + line + "】");
+		return null;
+	}
+
+}
---
+++ src/main/java/com/zhouzh3/pdf/Scholar.java
@@ -0,0 +1,131 @@
+package com.zhouzh3.pdf;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+
+import com.itextpdf.text.DocumentException;
+import com.itextpdf.text.pdf.PdfReader;
+import com.itextpdf.text.pdf.PdfStamper;
+import com.itextpdf.text.pdf.SimpleBookmark;
+
+public class Scholar {
+
+	public static void main(String[] args) throws IOException, DocumentException {
+		// printBookmark("src/main/resources/Packt.Mastering.Hadoop.1783983647.pdf");
+
+		String SRC = "src/main/resources/Hive编程指南.pdf";
+
+		Scholar scholar = new Scholar();
+		scholar.bookmark(SRC, 22);
+
+	}
+
+	public List<HashMap<String, Object>> readBookmark(String file, int step) throws IOException {
+		return readBookmark(new File(file), step);
+	}
+
+	public List<HashMap<String, Object>> readBookmark(File file, int step) throws IOException {
+		ArrayList<HashMap<String, Object>> retVal = new ArrayList<HashMap<String, Object>>();
+
+		List<String> readLines = readLines(file, "UTF-8");
+		for (String line : readLines) {
+			Bookmark bookmark = Bookmark.buildBookmark(line, step);
+			if (bookmark != null) {
+				int level = bookmark.getLevel();
+				// System.out.println(bookmark.getTitle() + "== " +
+				// bookmark.getLevel());
+				ArrayList<HashMap<String, Object>> findParent = findParent(retVal, level);
+				findParent.add(bookmark.getEntry());
+			}
+		}
+
+		return retVal;
+	}
+
+	@SuppressWarnings("unchecked")
+	private ArrayList<HashMap<String, Object>> findParent(ArrayList<HashMap<String, Object>> retVal, int level) {
+		ArrayList<HashMap<String, Object>> parent = retVal;
+		for (int i = 1; i < level; i++) {
+			// System.out.println("parent.size(): " + parent.size());
+			HashMap<String, Object> hashMap = parent.get(parent.size() - 1);
+
+			if (hashMap.containsKey("Kids")) {
+				parent = (ArrayList<HashMap<String, Object>>) hashMap.get("Kids");
+			} else {
+				parent = new ArrayList<HashMap<String, Object>>();
+				hashMap.put("Kids", parent);
+			}
+		}
+
+		return parent;
+
+	}
+
+	public List<String> readLines(String fileName) throws IOException {
+		return readLines(new File(fileName), "UTF-8");
+	}
+
+	private List<String> readLines(File file, String charset) throws IOException {
+		List<String> retVal = new ArrayList<String>();
+
+		BufferedReader br = new BufferedReader(new InputStreamReader(new FileInputStream(file), charset));
+		String line = null;
+		while ((line = br.readLine()) != null) {
+			if (line.trim().length() > 0) {
+				retVal.add(line);
+			}
+		}
+		br.close();
+		return retVal;
+	}
+
+	public void printBookmark(String fileName) throws IOException {
+		String SRC = fileName;
+		PdfReader reader = new PdfReader(SRC);
+
+		List<HashMap<String, Object>> bookmark = SimpleBookmark.getBookmark(reader);
+		for (HashMap<String, Object> hashMap : bookmark) {
+			for (String key : hashMap.keySet()) {
+				System.out.println("【" + key + "】, 【" + hashMap.get(key) + "】");
+			}
+			System.out.println("==================================");
+		}
+
+		reader.close();
+	}
+
+	protected void bookmark(String pdfFileName, int step) throws IOException, DocumentException {
+		bookmark(pdfFileName, getBookmarkFileName(pdfFileName), step);
+	}
+
+	protected void bookmark(String pdfFileName, String bookmarkFileName, int step)
+			throws IOException, DocumentException {
+		File DEST = getDest(pdfFileName);
+		PdfReader reader = new PdfReader(pdfFileName);
+		PdfStamper stamper = new PdfStamper(reader, new FileOutputStream(DEST));
+
+		List<HashMap<String, Object>> outlines = readBookmark(bookmarkFileName, step);
+
+		stamper.setOutlines(outlines);
+		stamper.close();
+	}
+
+	private String getBookmarkFileName(String fileName) {
+		File file = new File(fileName);
+		return new File(file.getParent(), file.getName().replaceAll("\\.\\w+$", ".txt")).getAbsolutePath();
+	}
+
+	private File getDest(String fileName) {
+		File file = new File(fileName);
+		return new File(file.getParent() + "/results/",
+				file.getName().replaceAll("\\.\\w+$", "_" + System.currentTimeMillis() + "$0"));
+	}
+
+}
